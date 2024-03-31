c4047cf07984262a4adfbee42bb461ae0909cd80
Fagr Ahmed <60551211+fagrahmed@users.noreply.github.com>
2024-03-31 10:37:49 +0200

    Update transactions_fact.sql
    adding date and time dimensions

--- models/transactions_fact.sql
+++ models/transactions_fact.sql
@@ -306,7 +306,9 @@ LEFT JOIN {{source('dbt-dimensions', 'wallets_dimension')}} wd ON td.walletdetai
 LEFT JOIN {{source('dbt-dimensions', 'clients_dimension')}} cd ON td.clientdetails ->> 'clientId' = cd.clientid
 LEFT JOIN {{source('dbt-dimensions', 'employees_dimension')}} ed ON wd.walletnumber = ed.employee_mobile AND
             td.transaction_createdat_utc2 between employee_createdat and employee_deletedat
-LEFT JOIN "dbt-dimensions".profiles_dimension pd ON wd.profileid = pd.walletprofileid AND wd.partnerid = pd.partnerid
+LEFT JOIN {{source('dbt-dimensions', 'profiles_dimension')}} pd ON wd.profileid = pd.walletprofileid AND wd.partnerid = pd.partnerid
+LEFT JOIN {{source('dbt-dimensions', 'date_dimension')}} dd ON DATE(td.transaction_modifiedat_utc2) = dd.full_date
+LEFT JOIN {{source('dbt-dimensions', 'time_dimension')}} tid ON TO_CHAR(td.transaction_modifiedat_utc2, 'HH24:MI:00') = TO_CHAR(tid.full_time, 'HH24:MI:SS')
 LEFT join cost_table ct on td.txndetailsid = ct.txndetailsid
 LEFT join revenue_table rt on td.txndetailsid = rt.txndetailsid
 GROUP BY td.txndetailsid, td.transaction_createdat_utc2, td.transaction_modifiedat_utc2, td.walletdetailsid, ed.clientid, ed.employee_id, td.txntype, td.transactionstatus, td.transactiondomain, td.interchangeaction, td.interchange_amount, td.amount, td.service_fees, td.hasservicefees, td.transactionreference, td.isreversedflag,
