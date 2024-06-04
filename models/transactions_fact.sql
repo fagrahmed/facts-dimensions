3d659d6451565c3d3b12438830cbdbc7b73ecf72
frefaat <fagrahmed@gmail.com>
2024-06-04 17:36:41 +0300

    schema.yml file for pk and fk, incremental fact table

--- models/transactions_fact.sql
+++ models/transactions_fact.sql
@@ -1,16 +1,14 @@
+-- models/transactions_fact.sql
 
-  
-    
+{{ config(        
+        materialized="incremental",
+        unique_key= "id",
+        on_schema_change='fail'
+) }}
 
-  create  table "dbt"."dbt-facts"."transactions_fact__dbt_tmp"
-  
-  
-    as
-  
-  (
-    -- models/transactions_fact.sql
-
-
+{% set table_exists_query = "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'dbt-facts' AND table_name = 'transactions_fact')" %}
+{% set table_exists_result = run_query(table_exists_query) %}
+{% set table_exists = table_exists_result.rows[0][0] if table_exists_result and table_exists_result.rows else False %}
 
 WITH cost_table AS (
          SELECT 
@@ -30,8 +28,8 @@ WITH cost_table AS (
              CASE WHEN lower(interchangeaction) = 'debit' THEN CAST(interchange_amount AS DECIMAL(10, 2)) + CAST(mc.vat * mc.amount AS DECIMAL(10, 2))
                  ELSE CAST(mc.amount AS DECIMAL(10, 2)) END as total_cost_after_vat
 
-        FROM "dbt"."dbt-dimensions"."transactions_dimension" td
-        JOIN "dbt"."public"."meeza_cost" mc on td.txntype = mc.transactiontype AND td.transactiondomain = mc.transactiondomain
+        FROM {{source('dbt-dimensions', 'transactions_dimension')}} td
+        JOIN {{source('dbt-dimensions', 'meeza_cost')}} mc on td.txntype = mc.transactiontype AND td.transactiondomain = mc.transactiondomain
         WHERE txntype in
                 ('TransactionTypes_RECEIVE_P2P', 'TransactionTypes_ATM_CASH_OUT', 'TransactionTypes_ATM_CASH_IN',
                  'TransactionTypes_RECEIVE_AGENT_CASH_IN', 'TransactionTypes_RECEIVE_DEPOSIT',
@@ -57,7 +55,7 @@ WITH cost_table AS (
             (CASE WHEN transactiondomain = 'TransactionDomains_OFF_US' THEN service_fees / 2
               ELSE 0 END) as total_cost_after_vat
 
-        FROM "dbt"."dbt-dimensions"."transactions_dimension" td
+        FROM {{source('dbt-dimensions', 'transactions_dimension')}} td
         WHERE txntype in ('TransactionTypes_BILL_PAYMENT', 'TransactionTypes_TOP_UP')
         AND transactionstatus IN ('TransactionStatus_POSTED', 'TransactionStatus_PENDING_ADVICE')
         
@@ -78,10 +76,10 @@ WITH cost_table AS (
             COALESCE(bt.corepayfees::float, 0) + COALESCE(bp.discount::float, 0) as total_cost_before_vat,
             COALESCE(bt.corepayfees::float, 0) + COALESCE(bp.discount::float, 0) as total_cost_after_vat
 
-        FROM "dbt"."dbt-dimensions"."transactions_dimension" td
-        LEFT JOIN "dbt"."axis_sme"."bankpaymenttransactions" bt ON td.txndetailsid = bt.originaltransactionid
-        LEFT JOIN "dbt"."axis_sme"."bankpayments" bp ON bt.bankpaymentid = bp.bankpaymentid
-        LEFT JOIN "dbt"."public"."txn_proc_cost_table" tp ON td.txntype = tp.transactiontype
+        FROM {{source('dbt-dimensions', 'transactions_dimension')}} td
+        LEFT JOIN {{source('axis_sme', 'bankpaymenttransactions')}} bt ON td.txndetailsid = bt.originaltransactionid
+        LEFT JOIN {{source('axis_sme', 'bankpayments')}} bp ON bt.bankpaymentid = bp.bankpaymentid
+        LEFT JOIN {{source('dbt-dimensions', 'txn_proc_cost_table')}} tp ON td.txntype = tp.transactiontype
               AND td.transaction_createdat_utc2 between tp.createdat and COALESCE(tp.endedat, now())
 
         WHERE txntype = 'TransactionTypes_SEND_BANK_PAYMENT'
@@ -106,9 +104,9 @@ WITH cost_table AS (
             coalesce(employeefeesdiscount::float, 0) + coalesce(transactionfeesdiscount::float, 0) +
             CAST(mc.vat * mc.amount AS DECIMAL(10, 2))                                as total_cost_after_vat
 
-        FROM "dbt"."dbt-dimensions"."transactions_dimension" td
-        LEFT JOIN "dbt"."public"."meeza_cost" mc ON td.txntype = mc.transactiontype AND td.transactiondomain = mc.transactiondomain
-        LEFT JOIN "dbt"."axis_sme"."disbursementtransactions" dt ON td.transactionreference = dt.wallettransactionreference
+        FROM {{source('dbt-dimensions', 'transactions_dimension')}} td
+        LEFT JOIN {{source('dbt-dimensions', 'meeza_cost')}} mc ON td.txntype = mc.transactiontype AND td.transactiondomain = mc.transactiondomain
+        LEFT JOIN {{source('axis_sme', 'disbursementtransactions')}} dt ON td.transactionreference = dt.wallettransactionreference
         WHERE txntype = 'TransactionTypes_SEND_SME_DEPOSIT'
             AND transactionstatus IN ('TransactionStatus_POSTED', 'TransactionStatus_PENDING_ADVICE')
 ),
@@ -127,9 +125,9 @@ revenue_table AS (
             coalesce(bt.axisfees_aibyte_transform::float, 0) as total_revenue_before_vat,
             coalesce(bt.axisfees_aibyte_transform::float, 0) * coalesce(bp.vat::float, 1) as total_revenue_after_vat
 
-        FROM "dbt"."dbt-dimensions"."transactions_dimension" td
-        LEFT JOIN "dbt"."axis_sme"."bankpaymenttransactions" bt ON td.txndetailsid = bt.originaltransactionid
-        LEFT JOIN "dbt"."axis_sme"."bankpayments" bp ON bt.bankpaymentid = bp.bankpaymentid
+        FROM {{source('dbt-dimensions', 'transactions_dimension')}} td
+        LEFT JOIN {{source('axis_sme', 'bankpaymenttransactions')}} bt ON td.txndetailsid = bt.originaltransactionid
+        LEFT JOIN {{source('axis_sme', 'bankpayments')}} bp ON bt.bankpaymentid = bp.bankpaymentid
         WHERE txntype = 'TransactionTypes_SEND_BANK_PAYMENT' AND transactionstatus IN ('TransactionStatus_POSTED', 'TransactionStatus_PENDING_ADVICE') AND isreversedflag = false
 
         UNION
@@ -146,8 +144,8 @@ revenue_table AS (
             coalesce(employeefees::float, 0) + coalesce(fees_aibyte_transform::float, 0) as total_revenue_before_vat,
             (coalesce(employeefeesvat_aibyte_transform::float, 1) + coalesce(transactionfeesvat_aibyte_transform::float, 1)) * (coalesce(employeefees::float, 0) + coalesce(fees_aibyte_transform::float, 0)) as total_revenue_after_vat
 
-        FROM "dbt"."dbt-dimensions"."transactions_dimension" td
-        LEFT JOIN "dbt"."axis_sme"."disbursementtransactions" dt ON td.transactionreference = dt.wallettransactionreference
+        FROM {{source('dbt-dimensions', 'transactions_dimension')}} td
+        LEFT JOIN {{source('axis_sme', 'disbursementtransactions')}} dt ON td.transactionreference = dt.wallettransactionreference
         WHERE txntype = 'TransactionTypes_SEND_SME_DEPOSIT'
                 AND transactionstatus IN ('TransactionStatus_POSTED', 'TransactionStatus_PENDING_ADVICE')
 
@@ -166,8 +164,8 @@ revenue_table AS (
             (coalesce(employeefeesvat_aibyte_transform::float, 1) + coalesce(transactionfeesvat_aibyte_transform::float, 1)) * (coalesce(employeefees::float, 0) + coalesce(fees_aibyte_transform::float, 0)) as total_revenue_after_vat
 
 
-        FROM "dbt"."dbt-dimensions"."transactions_dimension" td
-        LEFT JOIN "dbt"."axis_sme"."disbursementtransactions" dt ON td.transactionreference = dt.wallettransactionreference
+        FROM {{source('dbt-dimensions', 'transactions_dimension')}} td
+        LEFT JOIN {{source('axis_sme', 'disbursementtransactions')}} dt ON td.transactionreference = dt.wallettransactionreference
         WHERE txntype = 'TransactionTypes_SEND_SME_PAYROLL_DEPOSIT'
                 AND transactionstatus IN ('TransactionStatus_POSTED', 'TransactionStatus_PENDING_ADVICE')
 
@@ -185,7 +183,7 @@ revenue_table AS (
             coalesce(td.amount::float, 0) AS total_revenue_before_vat,
             coalesce(td.amount::float, 0) AS total_revenue_after_vat
 
-        FROM "dbt"."dbt-dimensions"."transactions_dimension" td
+        FROM {{source('dbt-dimensions', 'transactions_dimension')}} td
         WHERE txntype = 'TransactionTypes_SEND_SME_SUBSCRIPTION_PAYMENT'
           AND transactionstatus IN ('TransactionStatus_POSTED', 'TransactionStatus_PENDING_ADVICE')
           AND isreversedflag = false
@@ -206,7 +204,7 @@ revenue_table AS (
             (CASE WHEN lower(interchangeaction) = 'credit' AND transactiondomain = 'TransactionDomains_OFF_US'
                 THEN interchange_amount ELSE 0  END) as total_revenue_ater_vat
 
-        FROM "dbt"."dbt-dimensions"."transactions_dimension" td
+        FROM {{source('dbt-dimensions', 'transactions_dimension')}} td
         WHERE txntype = 'TransactionTypes_SEND_P2P'
             AND transactionstatus IN ('TransactionStatus_POSTED', 'TransactionStatus_PENDING_ADVICE')
 
@@ -226,7 +224,7 @@ revenue_table AS (
             (CASE WHEN lower(interchangeaction) = 'credit' AND hasservicefees = true
                 THEN interchange_amount + (service_fees::float) ELSE coalesce(service_fees::float, 0) END) as total_revenue_after_vat
 
-        FROM "dbt"."dbt-dimensions"."transactions_dimension" td
+        FROM {{source('dbt-dimensions', 'transactions_dimension')}} td
         WHERE txntype in ('TransactionTypes_ATM_CASH_OUT', 'TransactionTypes_ATM_CASH_IN', 'TransactionTypes_ATM_CASH_OUT_REVERSAL', 'TransactionTypes_ATM_CASH_IN_REVERSAL')
             AND transactionstatus IN ('TransactionStatus_POSTED', 'TransactionStatus_PENDING_ADVICE')
 
@@ -244,7 +242,7 @@ revenue_table AS (
             (service_fees::float) as total_revenue_before_vat,
             (service_fees::float) as total_revenue_after_vat
 
-        FROM "dbt"."dbt-dimensions"."transactions_dimension" td
+        FROM {{source('dbt-dimensions', 'transactions_dimension')}} td
         WHERE txntype in ('TransactionTypes_BILL_PAYMENT', 'TransactionTypes_TOP_UP')
             AND transactionstatus IN ('TransactionStatus_POSTED', 'TransactionStatus_PENDING_ADVICE')
 
@@ -262,7 +260,7 @@ revenue_table AS (
             td.amount as total_revenue_before_vat,
             td.amount as total_revenue_after_vat
 
-        FROM "dbt"."dbt-dimensions"."transactions_dimension" td
+        FROM {{source('dbt-dimensions', 'transactions_dimension')}} td
         WHERE txntype = 'TransactionTypes_CREATE_VCN_FEES'
             AND transactionstatus IN ('TransactionStatus_POSTED', 'TransactionStatus_PENDING_ADVICE')
 
@@ -280,46 +278,44 @@ revenue_table AS (
             CASE WHEN hasservicefees = true THEN td.service_fees ELSE 0 END as total_revenue_before_vat,
             CASE WHEN hasservicefees = true THEN td.service_fees ELSE 0 END as total_revenue_after_vat
 
-        FROM "dbt"."dbt-dimensions"."transactions_dimension" td
+        FROM {{source('dbt-dimensions', 'transactions_dimension')}} td
         WHERE txntype = 'TransactionTypes_SEND_REDEEM_SME_INADVANCE_DEPOSIT'
             AND transactionstatus IN ('TransactionStatus_POSTED', 'TransactionStatus_PENDING_ADVICE')
 )
 SELECT
-    td.txndetailsid,
-    td.transaction_createdat_utc2,
-    td.transaction_modifiedat_utc2,
-    td.walletdetailsid,
-    coalesce(td.clientdetails ->> 'clientId', ed.clientid) as clientid,
-    cd.clienttype,
-    pd.walletprofileid,
-    pd.partner_name,
-    (CASE WHEN ed.employee_id IS NULL THEN false ELSE true END) as is_employee,
-    ed.employee_id,
-    td.txntype,
-    td.transactionstatus,
-    td.transactiondomain,
-    td.interchangeaction,
-    td.interchange_amount,
+    md5(random()::text || clock_timestamp()::text) as id,
+    td.id AS txn_key,
+    dd.date_id AS date_key,
+    tid.time_id AS time_key,
+    wd.id AS wallet_key,
+    cd.id AS client_key,
+    pd.id AS profile_key,
+    ed.id AS employee_key,
+    (CASE WHEN ed.employee_id IS NULL THEN false ELSE true END) as is_employee,     
     td.amount,
-    td.service_fees,
-    td.hasservicefees,
-    td.transactionreference,
-    td.isreversedflag,
     ROUND(coalesce(ct.total_cost_before_vat, 0)::numeric, 2) as total_cost_before_vat,
     ROUND(coalesce(rt.total_revenue_before_vat, 0)::numeric, 2) as total_revenue_before_vat,
     ROUND(coalesce(ct.total_cost_after_vat, 0)::numeric, 2) as total_cost_after_vat,
     ROUND(coalesce(rt.total_revenue_after_vat, 0)::numeric, 2) as total_revenue_after_vat,
-    ROUND(SUM(COALESCE(rt.total_revenue_before_vat, 0) - COALESCE(ct.total_cost_before_vat, 0))::numeric, 2) as total_profit
+    ROUND(SUM(COALESCE(rt.total_revenue_before_vat, 0) - COALESCE(ct.total_cost_before_vat, 0))::numeric, 2) as total_profit,
+    (now()::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') as loaddate
 
-FROM "dbt"."dbt-dimensions"."transactions_dimension" td
-LEFT JOIN "dbt"."dbt-dimensions"."wallets_dimension" wd ON td.walletdetailsid = wd.walletid
-LEFT JOIN "dbt"."dbt-dimensions"."clients_dimension" cd ON td.clientdetails ->> 'clientId' = cd.clientid
-LEFT JOIN "dbt"."dbt-dimensions"."employees_dimension" ed ON wd.walletnumber = ed.employee_mobile AND
-            td.transaction_createdat_utc2 between employee_createdat and employee_deletedat
-LEFT JOIN "dbt-dimensions".profiles_dimension pd ON wd.profileid = pd.walletprofileid AND wd.partnerid = pd.partnerid
+
+FROM {{source('dbt-dimensions', 'transactions_dimension')}} td
+LEFT JOIN {{source('dbt-dimensions', 'wallets_dimension')}} wd ON td.walletdetailsid = wd.walletid
+LEFT JOIN {{source('dbt-dimensions', 'employees_dimension')}} ed ON wd.walletnumber = ed.employee_mobile AND
+            td.transaction_createdat_utc2 between employee_createdat_utc2 and employee_deletedat_utc2
+LEFT JOIN {{source('dbt-dimensions', 'clients_dimension')}} cd ON td.clientdetails ->> 'clientId' = cd.clientid
+LEFT JOIN {{source('dbt-dimensions', 'profiles_dimension')}} pd ON wd.profileid = pd.walletprofileid AND wd.partnerid = pd.partnerid
+LEFT JOIN {{source('dbt-dimensions', 'date_dimension')}} dd ON DATE(td.transaction_modifiedat_utc2) = dd.full_date
+LEFT JOIN {{source('dbt-dimensions', 'time_dimension')}} tid ON TO_CHAR(td.transaction_modifiedat_utc2, 'HH24:MI:00') = TO_CHAR(tid.full_time, 'HH24:MI:SS')
 LEFT join cost_table ct on td.txndetailsid = ct.txndetailsid
 LEFT join revenue_table rt on td.txndetailsid = rt.txndetailsid
-GROUP BY td.txndetailsid, td.transaction_createdat_utc2, td.transaction_modifiedat_utc2, td.walletdetailsid, ed.clientid, ed.employee_id, td.txntype, td.transactionstatus, td.transactiondomain, td.interchangeaction, td.interchange_amount, td.amount, td.service_fees, td.hasservicefees, td.transactionreference, td.isreversedflag,
-         ct.total_cost_before_vat, rt.total_revenue_before_vat, ct.total_cost_after_vat, rt.total_revenue_after_vat, pd.walletprofileid, pd.partner_name, cd.clienttype, td.clientdetails
-  );
-  
+
+{% if is_incremental() and table_exists %}
+    WHERE td.loaddate > COALESCE((SELECT max(loaddate::timestamptz) FROM {{ source('dbt-facts', 'transactions_fact') }}), '1900-01-01'::timestamp)
+{% endif %}
+
+GROUP BY td.amount, ed.employee_id,
+         ct.total_cost_before_vat, rt.total_revenue_before_vat, ct.total_cost_after_vat, rt.total_revenue_after_vat,
+         dd.date_id, tid.time_id,  ed.id, pd.id, wd.id, cd.id, td.id
